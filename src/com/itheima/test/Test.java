2d238fb838b3fe5630ea8a59f72f3b51fc3dba65
xiaosong <526098381@qq.com>
2019-03-11 09:40:42 +0800

    第一次提交

---
+++ src/com/itheima/test/Test.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Date 2019/3/11 9:39
+ * @Version 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("aaaa");
+    }
+}
