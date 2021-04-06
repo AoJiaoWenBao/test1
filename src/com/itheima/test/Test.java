d343961c1d5c660de3f67e4ad39d43c14400c5a4
88413427 <742666897@qq.com>
2021-04-06 15:56:59 +0800

    测试

--- src/com/itheima/test/Test.java
+++ src/com/itheima/test/Test.java
@@ -11,5 +11,8 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
     }
 }
