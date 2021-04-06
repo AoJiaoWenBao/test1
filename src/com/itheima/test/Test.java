9fd67bf5f8028c6b492c68f2be8aa13e6f0f42be
88413427 <742666897@qq.com>
2021-04-06 16:18:12 +0800

    测试2

--- src/com/itheima/test/Test.java
+++ src/com/itheima/test/Test.java
@@ -11,8 +11,5 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("aaaa");
-        System.out.println("aaaa");
-        System.out.println("aaaa");
-        System.out.println("aaaa");
     }
 }
